1cb2a257ad7195c48b13fef3ee6d4cb11de7bd4e
Eric Tran <tung.tran@naustud.io>
2018-02-05 10:09:26 +0700

    rename imgur to goalify

---
+++ accounts-goalify.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * Register this service (boilerplate).
+ */
+Accounts.oauth.registerService('goalify');
+
+/**
+ * Client functionality (boilerplate).
+ */
+if (Meteor.isClient) {
+	Meteor.loginWithGoalify = function(options, callback) {
+		/**
+		 * support (options, callback) and (callback)
+		 */
+		if (!callback && typeof options === 'function') {
+			callback = options;
+			options = null;
+		}
+
+		/**
+		 *
+		 */
+		var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
+		Goalify.requestCredential(options, credentialRequestCompleteCallback);
+	};
+
+	/**
+	 * Server functionality (boilerplate).
+	 * Ensures sanity of published user object.
+	 */
+} else {
+	Accounts.addAutopublishFields({
+		forLoggedInUser: _.map(
+			/**
+			 * Logged in user gets whitelisted fields + accessToken + expiresAt.
+			 */
+			Goalify.whitelistedFields.concat(['accessToken', 'expiresAt']), // don't publish refresh token
+			function(subfield) {
+				return 'services.goalify.' + subfield;
+			}
+		),
+
+		forOtherUsers: _.map(
+			/**
+			 * Other users get whitelisted fields without emails, because even with
+			 * autopublish, no legitimate web app should be publishing all users' emails.
+			 */
+			_.without(Goalify.whitelistedFields, 'email', 'verified_email'),
+			function(subfield) {
+				return 'services.goalify.' + subfield;
+			}
+		),
+	});
+}
--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'tunguska:accounts-imgur',
+  name: 'erictran:accounts-goalify',
   version: '0.0.1',
-  summary: 'OAuth2 for Imgur',
-  git: 'https://github.com/robfallows/tunguska-accounts-imgur',
+  summary: 'OAuth2 for Goalify',
+  git: 'https://github.com/goalifyplus/meteor-accounts-goalify',
   documentation: 'README.md'
 });
 
@@ -15,9 +15,9 @@ Package.onUse(function(api) {
   api.imply('accounts-base', ['client', 'server']);
 
   api.use('accounts-oauth', ['client', 'server']);
-  api.use('tunguska:imgur@0.0.1', ['client', 'server']);
+  api.use('erictran:goalify@0.0.1', ['client', 'server']);
 
-  api.addFiles('accounts-imgur_login_button.css', 'client');
+  api.addFiles('accounts-goalify_login_button.css', 'client');
 
-  api.addFiles('accounts-imgur.js');
+  api.addFiles('accounts-goalify.js');
 });
